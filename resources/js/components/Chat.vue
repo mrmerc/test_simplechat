850c1f6f38da29c6db13588b1f8b8a7df09e676e
Vasya <merc.l33t@gmail.com>
2019-10-10 16:18:27 +0300

    Frontend scaffold

--- resources/js/components/Chat.vue
+++ resources/js/components/Chat.vue
@@ -1,10 +1,21 @@
 <template>
-	<div id="chat-container">
+	<div id="chat-container" class="container border p-2">
 		<div id="feed">
-
+			<ul class="list-group">
+				<li class="list-group-item">12:40 Вася: Привет</li>
+				<li class="list-group-item">12:41 Петя: Приветик</li>
+				<li class="list-group-item">12:41 Петя: Приветик</li>
+				<li class="list-group-item">12:41 Петя: Приветик</li>
+				<li class="list-group-item">12:41 Петя: Приветик</li>
+				<li class="list-group-item">12:41 Петя: Приветик</li>
+				<li class="list-group-item">12:41 Петя: Приветик</li>
+				<li class="list-group-item">12:41 Петя: Приветик</li>
+				<li class="list-group-item">12:41 Петя: Приветик</li>
+			</ul>
 		</div>
 		<div id="composer">
-
+			<textarea placeholder="Сообщение" rows="1"></textarea>
+			<input class="btn btn-success m-1" type="button" value="Отправить">
 		</div>
 	</div>
 </template>
@@ -33,5 +44,34 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+	.container {
 
+		#feed {
+			height: 300px;
+			overflow-x: hidden;
+			overflow-y: scroll;
+
+			ul {
+				list-style: none;
+				display: flex;
+				flex-direction: column-reverse;
+			}
+		}
+
+		#composer {
+			height: 50px;
+			display: flex;
+			align-items: center;
+
+			textarea {
+				width: 80%;
+				resize: none;
+				padding-left: 8px;
+			}
+
+			input {
+				width: 20%;
+			}
+		}
+	}
 </style>
